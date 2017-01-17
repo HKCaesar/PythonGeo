479a179eea24a9a5b07f86c8b2adad83038613f7
sn2234 <sn2234@gmail.com>
2017-01-18 01:49:54 +0200

    First test with U-Net

---
+++ PythonGeo/Models.py
@@ -0,0 +1,92 @@
+
+import SetEnvForGpu
+
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Activation, Flatten
+from keras.layers import Convolution2D, MaxPooling2D, LocallyConnected2D
+
+# UNet
+from keras.models import Model
+from keras.layers import Input, merge, Convolution2D, MaxPooling2D, UpSampling2D, Reshape, core, Dropout
+from keras.optimizers import Adam
+from keras.callbacks import ModelCheckpoint, LearningRateScheduler
+from keras import backend as K
+#from keras.utils.visualize_util import plot
+from keras.optimizers import SGD
+
+
+def getSimpleModel(nb_classes, input_shape):
+
+    # number of convolutional filters to use
+    nb_filters = 32
+    # size of pooling area for max pooling
+    pool_size = (2, 2)
+    # convolution kernel size
+    kernel_size = (3, 3)
+
+    model = Sequential()
+
+    model.add(Convolution2D(nb_filters, kernel_size[0], kernel_size[1],
+                            border_mode='valid',
+                            input_shape=input_shape))
+    model.add(Activation('relu'))
+    model.add(Convolution2D(nb_filters, kernel_size[0], kernel_size[1]))
+    model.add(Activation('relu'))
+    model.add(Dropout(0.3))
+
+    model.add(Flatten())
+    model.add(Dense(128))
+    model.add(Activation('relu'))
+    model.add(Dropout(0.3))
+    model.add(Dense(64))
+    model.add(Activation('relu'))
+    model.add(Dropout(0.3))
+    model.add(Dense(nb_classes))
+    model.add(Activation('softmax'))
+
+    model.compile(loss='categorical_crossentropy',
+                    optimizer='rmsprop',
+                    metrics=['accuracy'])
+
+    return model
+
+#Define the neural network (n_ch,patch_height,patch_width)
+def getUNet(input_shape, nb_classes):
+    (n_ch,patch_height,patch_width) = input_shape
+    inputs = Input(input_shape)
+    conv1 = Convolution2D(32, 3, 3, activation='relu', border_mode='same')(inputs)
+    conv1 = Dropout(0.2)(conv1)
+    conv1 = Convolution2D(32, 3, 3, activation='relu', border_mode='same')(conv1)
+    pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
+    #
+    conv2 = Convolution2D(64, 3, 3, activation='relu', border_mode='same')(pool1)
+    conv2 = Dropout(0.2)(conv2)
+    conv2 = Convolution2D(64, 3, 3, activation='relu', border_mode='same')(conv2)
+    pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
+    #
+    conv3 = Convolution2D(128, 3, 3, activation='relu', border_mode='same')(pool2)
+    conv3 = Dropout(0.2)(conv3)
+    conv3 = Convolution2D(128, 3, 3, activation='relu', border_mode='same')(conv3)
+
+    up1 = merge([UpSampling2D(size=(2, 2))(conv3), conv2], mode='concat', concat_axis=1)
+    conv4 = Convolution2D(64, 3, 3, activation='relu', border_mode='same')(up1)
+    conv4 = Dropout(0.2)(conv4)
+    conv4 = Convolution2D(64, 3, 3, activation='relu', border_mode='same')(conv4)
+    #
+    up2 = merge([UpSampling2D(size=(2, 2))(conv4), conv1], mode='concat', concat_axis=1)
+    conv5 = Convolution2D(32, 3, 3, activation='relu', border_mode='same')(up2)
+    conv5 = Dropout(0.2)(conv5)
+    conv5 = Convolution2D(32, 3, 3, activation='relu', border_mode='same')(conv5)
+    #
+    conv6 = Convolution2D(nb_classes, 1, 1, activation='relu',border_mode='same')(conv5)
+    conv6 = core.Reshape((nb_classes,patch_height*patch_width))(conv6)
+    conv6 = core.Permute((2,1))(conv6)
+    ############
+    conv7 = core.Activation('softmax')(conv6)
+
+    model = Model(input=inputs, output=conv7)
+
+    # sgd = SGD(lr=0.01, decay=1e-6, momentum=0.3, nesterov=False)
+    model.compile(optimizer='sgd', loss='categorical_crossentropy',metrics=['accuracy'])
+
+    return model
--- PythonGeo/UNetTest.py
+++ PythonGeo/UNetTest.py
@@ -1,4 +1,3 @@
-
 import SetEnvForGpu
 
 import itertools
@@ -26,11 +25,11 @@ def showImg(img):
 nb_classes = 11
 nb_epoch = 12
 
-img_dim_x=51
-img_dim_y=51
+img_dim_x=100
+img_dim_y=100
 input_shape = (3,img_dim_y,img_dim_x)
 
-model = Models.getSimpleModel(nb_classes, input_shape)
+model = Models.getUNet(input_shape, nb_classes)
 
 testId = '6100_1_3'
 (img_i, mask) = DataTools.loadAll(testId)
@@ -39,22 +38,17 @@ img = prc.scale(img.reshape(-1, 1)).reshape(img_i.shape)
 
 gall = ImageUtils.genPatches(img.shape[1:], (img_dim_y, img_dim_x), 10)
 gg = itertools.islice(gall, 2000)
-(imgs, classes, masks) = ImageUtils.prepareDataSets(gall, img, mask)
+(imgs, classes, masks) = ImageUtils.prepareDataSets(gg, img, mask)
 
-(x_train, x_cv, y_train, y_cv) = scv.train_test_split(imgs, classes, test_size=0.2)
-y_train_cat = np_utils.to_categorical(y_train, nb_classes)
-y_cv_cat = np_utils.to_categorical(y_cv, nb_classes)
+(x_train, x_cv, y_train, y_cv) = scv.train_test_split(imgs, masks, test_size=0.2)
+y_train_cat = np_utils.to_categorical(y_train.flatten(), nb_classes)
+y_train_cat = y_train_cat.reshape((y_train.shape[0], y_train.shape[1]*y_train.shape[2], nb_classes))
 
-model.fit(x_train, y_train_cat, nb_epoch=15)
-#y_pred = model.predict(x_cv)
+model.fit(x_train, y_train, nb_epoch=10)
 
-score, acc = model.evaluate(x_cv, y_cv_cat)
-print("Accuracy on CV dataset: {0}".format(acc))
+y_cv_cat = np_utils.to_categorical(y_cv.flatten(), nb_classes)
+y_cv_cat = y_cv_cat.reshape((y_cv.shape[0], y_cv.shape[1]*y_cv.shape[2], nb_classes))
 
-x_len = ImageUtils.lenInPatches(img.shape[1:], (img_dim_y, img_dim_x), 10)
-x_all = imgs
-y_all = classes
-y_all_cat = np_utils.to_categorical(y_all, nb_classes)
-
-res = model.predict(x_all)
-img_res = np.argmax(res, axis=1).reshape(res.shape[0] // x_len, x_len)
+rez = model.predict(x_cv)
+xx = np.argmax(rez, axis = 2)
+xx = xx.reshape((-1, img_dim_y, img_dim_x))
