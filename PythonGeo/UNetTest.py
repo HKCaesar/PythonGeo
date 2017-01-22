89ba2cc115b6c200dd8dff92bf74839b1a839a89
sn2234 <sn2234@gmail.com>
2017-01-22 11:54:29 +0200

    Lrger U-Net model added

--- PythonGeo/UNetTest.py
+++ PythonGeo/UNetTest.py
@@ -25,30 +25,53 @@ def showImg(img):
 nb_classes = 11
 nb_epoch = 12
 
-img_dim_x=100
-img_dim_y=100
+img_dim_x=200
+img_dim_y=200
 input_shape = (3,img_dim_y,img_dim_x)
 
-model = Models.getUNet(input_shape, nb_classes)
+model = Models.getGnet(input_shape, nb_classes)
 
 testId = '6100_1_3'
 (img_i, mask) = DataTools.loadAll(testId)
 img = img_i.astype(float)
 img = prc.scale(img.reshape(-1, 1)).reshape(img_i.shape)
 
-gall = ImageUtils.genPatches(img.shape[1:], (img_dim_y, img_dim_x), 10)
+gall = ImageUtils.genPatches(img.shape[1:], (img_dim_y, img_dim_x), 60)
 gg = itertools.islice(gall, 2000)
-(imgs, classes, masks) = ImageUtils.prepareDataSets(gg, img, mask)
+(imgs, classes, masks) = ImageUtils.prepareDataSets(gall, img, mask)
 
 (x_train, x_cv, y_train, y_cv) = scv.train_test_split(imgs, masks, test_size=0.2)
 y_train_cat = np_utils.to_categorical(y_train.flatten(), nb_classes)
 y_train_cat = y_train_cat.reshape((y_train.shape[0], y_train.shape[1]*y_train.shape[2], nb_classes))
 
-model.fit(x_train, y_train, nb_epoch=10)
+#classWeights = { 0:0.2, 1:1.0, 2:1.0, 3:1.0, 4:1.0, 5:1.0, 6:1.0, 7:1.0, 8:1.0, 9:1.0, 10:1.0}
+
+# For UNet
+#model.fit(x_train, y_train_cat, nb_epoch=15, batch_size=11)
+
+# For GNet
+model.fit(x_train, y_train_cat, nb_epoch=5, batch_size=4)
+
+#trainBatchSize = 10
+#idx = 0
+#ii = 0
+#while idx < x_train.shape[0]:
+#    print("Training batch: {0}".format(ii))
+#    maxIdx = min(idx + trainBatchSize, x_train.shape[0])
+#    x_batch = x_train[idx:maxIdx]
+#    y_batch = y_train[idx:maxIdx]
+#    y_batch_cat = np_utils.to_categorical(y_batch.flatten(), nb_classes)
+#    y_batch_cat = y_batch_cat.reshape((y_batch.shape[0], y_batch.shape[1]*y_batch.shape[2], nb_classes))
+#    model.train_on_batch(x_batch, y_batch_cat)
+#    idx += trainBatchSize
+#    ii += 1
 
 y_cv_cat = np_utils.to_categorical(y_cv.flatten(), nb_classes)
 y_cv_cat = y_cv_cat.reshape((y_cv.shape[0], y_cv.shape[1]*y_cv.shape[2], nb_classes))
 
 rez = model.predict(x_cv)
-xx = np.argmax(rez, axis = 2)
+rez1 = np.array(rez)
+rez1[:,:,0:1] *= 0.08
+#rez1[:,:,0:1] = 0
+xx = np.argmax(rez1, axis = 2)
 xx = xx.reshape((-1, img_dim_y, img_dim_x))
